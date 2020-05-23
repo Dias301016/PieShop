c9ae628f34faa9bb2c50654805c0343203b5e4ab
Dias301016 <61219680+Dias301016@users.noreply.github.com>
2020-05-24 01:59:29 +0600

    first web app

---
+++ WebApplication5/WebApplication5/ViewModels/PiesListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication5.Models;
+
+namespace WebApplication5.ViewModels
+{ 
+    public class PiesListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; set; }
+
+        public string CurrentCategory { get; set; }
+    }
+}
